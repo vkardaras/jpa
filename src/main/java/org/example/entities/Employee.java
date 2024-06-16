6b44f9526c65807ba69b1ea2225050b85f9767af
Vasilis Kardaras <vasiliskardaras@gmail.com>
2024-06-16 17:43:14 +0300

    working with the context

--- src/main/java/org/example/entities/Employee.java
+++ src/main/java/org/example/entities/Employee.java
@@ -1,12 +1,15 @@
 package org.example.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Employee {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String address;
