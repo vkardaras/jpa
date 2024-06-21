3695c55dd1b7c46e0f67c97f8fc6eced9dd808b4
Vasilis Kardaras <vasiliskardaras@gmail.com>
2024-06-21 09:53:26 +0300

    criteria query

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -34,12 +34,19 @@ public class Main {
 //            cp.select(customerRoot); // SELECT c FROM Customer c
 //            TypedQuery<Customer> query = em.createQuery(cp);
 
-            CriteriaQuery<String> cp = builder.createQuery(String.class);
-            Root<Customer> customerRoot = cp.from(Customer.class);
-            cp.multiselect(customerRoot.get("name")); // SELECT c.name FROM Customer c
-            TypedQuery<String> query = em.createQuery(cp);
+//            CriteriaQuery<String> cp = builder.createQuery(String.class);
+//            Root<Customer> customerRoot = cp.from(Customer.class);
+//            cp.select(customerRoot.get("name")); // SELECT c.name FROM Customer c
+//            TypedQuery<String> query = em.createQuery(cp);
 
-            query.getResultList().forEach(System.out::println);
+            CriteriaQuery<Object[]> cp = builder.createQuery(Object[].class);
+            Root<Customer> customerRoot = cp.from(Customer.class);
+            cp.multiselect(customerRoot.get("name"), customerRoot.get("id")) // SELECT c.name, c.id FROM Customer c
+                    .where(builder.ge(customerRoot.get("id"), 3)) // WHERE c.id >= 5
+                    .orderBy(builder.desc(customerRoot.get("id"))); // ORDER BY c.id DESC
+            TypedQuery<Object[]> query = em.createQuery(cp);
+
+            query.getResultList().forEach(o -> System.out.println(o[0] + " " + o[1]));
 
             em.getTransaction().commit(); // end of transaction
         } finally {
